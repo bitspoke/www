7ae185dcf1ad29f48d7325a99ed3b5d113d3a3b5
pangioletti <paolo.angioletti@gmail.com>
2014-06-12 17:33:28 +0100

    Fix user/pass mongo credential issue

--- app/services/Mongo.scala
+++ app/services/Mongo.scala
@@ -14,11 +14,8 @@ class RealMongo extends Mongo {
   import com.mongodb.casbah.{MongoClientURI, MongoClient}
   import play.api.Play
 
-  val db = MongoClient(
-    MongoClientURI(
-      Play.current.configuration.getString("db.default.uri").getOrElse("mongodb://localhost")
-    )
-  )("bitspoke")
+  val uri = MongoClientURI(Play.current.configuration.getString("db.default.uri").getOrElse("mongodb://localhost/bitspoke"))
+  val db = MongoClient(uri).apply(uri.underlying.getDatabase)
 }
 
 
