eb5fb5a3a57a85760db7ed1f00e31a2f79018e96
pangioletti <paolo.angioletti@gmail.com>
2014-06-17 21:14:27 +0100

    Add AngularJS controller for articles

--- app/controllers/ArticleController.scala
+++ app/controllers/ArticleController.scala
@@ -1,16 +1,32 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.api.libs.json.{Json, Writes}
 import scaldi.Injector
 import scaldi.Injectable.inject
 import services.ArticleService
-
+import models.Article
 
 class ArticleController(implicit val i: Injector) extends Controller {
 
   val service = inject[ArticleService]
 
-  def list = Action {
-    Ok(views.html.articles(service.list))
+  def guiList = Action {
+    Ok(views.html.articles())
+  }
+
+  def apiList = Action {
+    Ok(Json.toJson(service.list))
+  }
+
+
+  implicit val articleWrites = new Writes[Article] {
+    def writes(article: Article) = Json.obj(
+      "title" -> article.title,
+      "author" -> article.author,
+      "epoch" -> article.epoch,
+      "summary" -> article.summary,
+      "content" -> article.content
+    )
   }
 }
