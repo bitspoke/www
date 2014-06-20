eace3017375c2b55ad3582ee3825cd2832fb9904
pangioletti <paolo.angioletti@gmail.com>
2014-06-20 22:47:00 +0100

    Save article in memory

--- test/assets/javascripts/unit/controllers.js
+++ test/assets/javascripts/unit/controllers.js
@@ -1,17 +1,33 @@
 "use strict";
 
 describe("ArticleCtrl", function() {
+  var scope;
 
-    // module() is used to load Angular modules that you want to test
-    beforeEach(module("bitspokeApp"));
+  // module() is used to load Angular modules that you want to test
+  beforeEach(function() {
+    module("bitspokeApp");
+    inject(function($controller) {
+      scope = {};
+      $controller("ArticleCtrl", {$scope: scope}); // new ArticleCtrl(scope);
+    });
+  });
 
 
-    // inject() is used to inject arguments of all given functions
+  // inject() is used to inject arguments of all given functions
 
-    it("should create 'articles' model with 3 articles", inject(function($controller) {
-        var scope = {},
-            ctrl = $controller("ArticleCtrl", {$scope: scope}); // new ArticleCtrl(scope);
+  it("should have no articles on init", inject(function($controller) {    
+    expect(scope.articlesIsEmpty()).toBe(true);    
+  }));
 
-        expect(scope.articles.length).toBe(3);
-    }));
+
+  it("should know when can save article", inject(function($controller) {    
+    expect(scope.canSaveArticle({"title":"this can be saved"})).toBe(true);
+    expect(scope.canSaveArticle({"content":"since it has no title then it cannot be saved"})).toBe(false);
+  }));
+
+
+  it("should save an article", inject(function($controller) {
+    scope.save({"title":"a new article"});
+    expect(scope.articles.length).toBe(1);
+  }));
 });
